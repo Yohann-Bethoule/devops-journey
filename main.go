7dfccea43474758423ecf097a0222335df0bc89b
Yohann Bethoule <ybethoule@figarocms.fr>
2022-10-14 15:47:26 +0200

    remove group for endpoints

--- main.go
+++ main.go
@@ -31,14 +31,11 @@ func main() {
 	r := gin.Default()
 
 	// API v1
-	v1 := r.Group("/api/v1")
-	{
-		v1.POST("/", createTodo)
-		v1.GET("/", fetchAllTodo)
-		v1.GET("/:id", fetchSingleTodo)
-		v1.PUT("/:id", updateTodo)
-		v1.DELETE("/:id", deleteTodo)
-	}
+	r.POST("/", createTodo)
+	r.GET("/", fetchAllTodo)
+	r.GET("/:id", fetchSingleTodo)
+	r.PUT("/:id", updateTodo)
+	r.DELETE("/:id", deleteTodo)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
